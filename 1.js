5290bcd079387097cab87a08cedc1028fec31948
NguyenSinh <sinhnd2000@gmail.com>
2022-05-31 15:56:10 +0700

    Bổ sung thêm (31/05/2022)

--- 1.js
+++ 1.js
@@ -13,4 +13,25 @@ $(function () {
         khoimonan.isotope({ filter: $(this).data('monan') })
         return false;
     })
+
+    new WOW().init();
 })
+
+document.addEventListener('DOMContentLoaded',function(){
+    var status = 'under100';
+    var navbar = document.getElementById('Menu');
+    window.addEventListener('scroll',function(){
+        if ( window.pageYOffset > 300){
+            if ( status == 'under100') {
+                status = 'over100';
+                navbar.classList.add('logovamenufixed');               
+            }
+        }
+        else if ( window.pageYOffset <= 300) {
+            if ( status == 'over100' ) {
+                status = 'under100';
+                navbar.classList.remove('logovamenufixed');               
+            }
+        }
+    })
+},false);
